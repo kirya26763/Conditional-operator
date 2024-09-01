654644efe2671e41ce68e14be07e224954f081c2
Arty <kirya26763@mail.ru>
2024-09-01 20:30:57 +0700

    homework

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,10 @@ public class Main {
         }
 
         {
-            int temperature = 10;
+            int temperature = 4;
             if (temperature < 5) {
                 System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
-            }
-            if (temperature >= 5) {
+            } else {
                 System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
             }
         }
@@ -32,61 +31,51 @@ public class Main {
             int age = 25;
             if (age >= 2 && age <= 6) {
                 System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
-            }
-            if (age >= 7 && age <= 17) {
+            } else if (age >= 7 && age <= 17) {
                 System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
-            }
-            if (age >= 18 && age <= 24) {
+            } else if (age >= 18 && age <= 24) {
                 System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в учиверситет.");
-            }
-            if (age > 24) {
+            } else if (age > 24) {
                 System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу.");
             }
         }
 
         {
             int age = 21;
-            if (age > 5 && age < 14) {
-                System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении");
-            }
             if (age < 5) {
                 System.out.println("Если возраст ребенка равен " + age + ", то нельзя кататься на аттракционе");
             }
-            if (age > 14) {
+            if (age >= 5 && age < 14) {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении");
+            }
+            if (age >= 14) {
                 System.out.println("Если возраст ребенка равен " + age + ", то он может кататься без сопровождения взрослого");
             }
         }
 
         {
-            int people = 100;
+            int people = 103;
             int capacity = 102;
             int seating = 60;
-            int standing = capacity - seating;
-            int availableSeats = capacity - people;
-            if (people >= capacity) {
-                System.out.println("Свободных мест нету");
-            } else {
-                System.out.println("Осталось " + availableSeats + " мест");
-            }
-            if (availableSeats > 60) {
-                System.out.println(standing + " стоячих и " + (availableSeats - standing) + " сидячих мест");
-            } else {
-                System.out.println(availableSeats + " стоячих мест");
+            if (people < seating) {
+                System.out.println("В вагоне " + (seating - people) + " сидячих мест");
+            } else if (people >= seating && people < capacity) {
+                System.out.println("В вагоне " + (capacity - people) + " стоячих мест");
+            } else if (people >= capacity) {
+                System.out.println("В вагоне нет мест");
             }
         }
 
         {
-            int one = 3;
-            int two = 19;
+            int one = 1;
+            int two = 1;
             int three = 1;
             if (one > two && one > three) {
                 System.out.println("самое большое число one " + one);
-            }
-            if (two > one && two > three) {
-                System.out.println("самое большое число two " + two);
-            }
-            if (three > one && three > two) {
-                System.out.println("самое большое число three " + three);
+            } else if (two > three) {
+                System.out.println("Самое большое число two " + two);
+            } else {
+                System.out.println("Самое большое число three " + three);
             }
         }
     }
